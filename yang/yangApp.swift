2dd2098572a7e9bde4c29b85674e02e3b9f1f90d
berrrrr <berrrrr1@gmail.com>
2025-05-18 15:58:41 +0900

    Initial Commit

---
+++ yang/yangApp.swift
@@ -0,0 +1,17 @@
+//
+//  yangApp.swift
+//  yang
+//
+//  Created by 박수진 on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct yangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ yangTests/yangTests.swift
@@ -0,0 +1,17 @@
+//
+//  yangTests.swift
+//  yangTests
+//
+//  Created by 박수진 on 5/18/25.
+//
+
+import Testing
+@testable import yang
+
+struct yangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
